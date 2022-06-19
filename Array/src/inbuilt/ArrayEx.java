53c51e991387379d72b4e5b617defd9f3e6ce86e
MeruguEshwar <merugueshwar93@gmail.com>
2022-06-19 18:33:55 +0530

    Sec msg

---
+++ Array/src/inbuilt/ArrayEx.java
@@ -0,0 +1,16 @@
+package inbuilt;
+
+public class ArrayEx 
+{
+	public static void main(String argv[])
+	{
+		int a[]= {1,2,3,4,5,7,8,9};
+		
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+		System.out.println(a[9]);
+	}
+}
